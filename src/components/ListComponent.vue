b21b86c0f84036d487aa0572d9029e2d3caf9169
amit-singh88598 <amitsingh992728@gmail.com>
2021-12-07 15:41:03 +0530

    added infinite scrolling

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="scrolling-component" ref="scrollComponent">
+    <post-component v-for="(post, index) in posts" :post="post" :key="index" />
+  </div>
+</template>
+
+<script>
+import getPosts from "../api/get-posts";
+import PostComponent from "./Post.vue";
+import { ref, onMounted, onUnmounted } from "vue";
+
+export default {
+  components: {
+    PostComponent,
+  },
+
+  setup() {
+    const posts = ref(getPosts(2));
+    const scrollComponent = ref(null);
+
+    const loadMorePosts = () => {
+      let newPosts = getPosts(2);
+      posts.value.push(...newPosts);
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+
+    const handleScroll = () => {
+      let element = scrollComponent.value;
+      if (element.getBoundingClientRect().bottom < window.innerHeight) {
+        loadMorePosts();
+      }
+    };
+
+    return {
+      posts,
+      scrollComponent,
+    };
+  },
+};
+</script>
